d153cf017261e2425648f69789cfc98e53fa6a4e
Ethan Chew <ethanchewmh@gmail.com>
2021-11-15 11:39:21 +0800

    Initial Commit

---
+++ ArrivalSG/ArrivalSGApp.swift
@@ -0,0 +1,20 @@
+//
+//  ArrivalSGApp.swift
+//  ArrivalSG
+//
+//  Created by Ethan Chew on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ArrivalSGApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
